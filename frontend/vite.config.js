efa69d21a97fb53172192c0d9caa5e584475f187
micha <michachael707@gmail.com>
2026-02-26 00:02:24 +0800

    some codes are updated to match with vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const REPO_NAME = 'ordermade';
+
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ordermade/',
   plugins: [react(),tailwindcss(),],
 })
